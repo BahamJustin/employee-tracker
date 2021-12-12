db154b7414c8d20a7b9b9f55e4ac09b4be3184a2
BahamJustin <bahamjustin@outlook.com>
2021-12-11 21:56:15 -0600

    create view routes

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,3 @@
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS employee;
+DROP DATABASE IF EXISTS  just_tech_news_db;
 
-
-CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-);
-
-CREATE TABLE roles (
-    id INTEGER PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10, 0),
-    department_id INTEGER,
-);
-
-CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL,
-  manager_id INTEGER NOT NULL,
-);
+CREATE DATABASE  just_tech_news_db;
